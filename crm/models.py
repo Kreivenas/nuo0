5ef1911b4293c8c7ee1c88383246b6e10aed0f60
kreivenas <kreivenastomas78@gmail.com>
2023-05-25 20:19:56 +0300

    admin puslapio kurimas1

--- crm/models.py
+++ crm/models.py
@@ -6,8 +6,14 @@ COUNTRES = (
     ("PL", "Poland"),
 )
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=1024)
     adress = models.CharField(max_length=1024)
     phone = models.CharField(max_length=15)
     country = models.CharField(max_length=2, choices=COUNTRES)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
