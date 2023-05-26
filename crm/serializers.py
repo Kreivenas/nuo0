a8d4023e353bc46594031b17635ab08959891884
kreivenas <kreivenastomas78@gmail.com>
2023-05-26 10:41:18 +0300

    prideta Category

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name", "active"]
 
 class CustomerSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = Customer
         fields = "__all__"
